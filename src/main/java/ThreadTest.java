423d07628c84b6c3b25e57322984aa2dca56fa39
Dreamer5201314 <1085011285@qq.com>
2022-10-05 18:41:51 +0800

    multi thread

---
+++ src/main/java/ThreadTest.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadTest {
+    private  List<String> msg = new ArrayList();
+    private List<String>  spliteList = null;
+
+
+    public ThreadTest(int  i) throws IOException {
+        String valueString = null;
+        InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream("src/main/resources/files/preResult.txt"));
+        BufferedReader br = new BufferedReader(inputStreamReader);
+        while ((valueString = br.readLine()) != null){
+            msg.add(valueString.trim());
+        }
+        br.close();
+    }
+
+    public List<String> getMsg() {
+        return msg;
+    }
+
+    public List<List<String>>  spliteList(int numThreads) throws IOException {
+
+        int length = msg.size();
+        // 计算可以分成多少组
+        int num =  length/numThreads;
+        List<List<String>> newList = new ArrayList<>(num);
+        if(num == 0){
+            newList.add(msg);
+        }else{
+            for (int i = 1; i <= numThreads; i++) {
+                // 开始位置
+                int fromIndex = (i-1)*num;
+                // 结束位置
+                int toIndex = i*num;
+                if(i==numThreads){
+                    toIndex = i*num+ length%numThreads;
+                }
+
+                newList.add(msg.subList(fromIndex,toIndex)) ;
+            }
+        }
+
+        return  newList ;
+    }
+
+    public void customerMsg(List<String> splitMsgList){
+        for(String str : splitMsgList){
+            System.out.println(Thread.currentThread().getName()+"----"+str);
+            try {
+                Thread.sleep(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) throws InterruptedException, IOException {
+        int numThreads = Runtime.getRuntime().availableProcessors();
+        //int numThreads = 10;
+        //初始化数据
+        ThreadTest test2 = new ThreadTest(113);
+        //拆分数据
+        List<List<String>>  tt = test2.spliteList(numThreads);
+        CountDownLatch latch =  new CountDownLatch( numThreads );
+        final ExecutorService exec = Executors.newFixedThreadPool(numThreads);
+        //多线程运行
+        long startTime=System.currentTimeMillis();
+        String keyword = "湖南";
+        int num=0;
+        for(int ii=0; ii<numThreads;ii++){
+            exec.submit(new MyThread(tt.get(ii),latch,"Threadzcy"+ii,keyword,String.format("hunan%s",num)));
+            num++;
+        }
+        latch.await();
+        exec.shutdown();
+        long endTime=System.currentTimeMillis(); //获取结束时间
+        System.out.println("程序运行时间： "+(endTime-startTime)+"ms");
+
+        long startTime1=System.currentTimeMillis();
+        List<String>  allMsgList = test2.getMsg();
+        for(String str : allMsgList){
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+        long endTime1=System.currentTimeMillis(); //获取结束时间
+        System.out.println("程序运行时间： "+(endTime1-startTime1)+"ms");
+
+    }
+
+}
--- src/main/java/searchData.java
+++ src/main/java/searchData.java
@@ -4,7 +4,7 @@ import java.net.URLDecoder;
 public class searchData {
 
     public static void search(String key,String filename) throws IOException {
-        /*
+
         //定义输入流，打开源数据集txt文件
         InputStreamReader inStream = new InputStreamReader(new FileInputStream(new File("src/main/resources/files/preResult.txt")), "UTF-8");
         //定义输出流，写入搜索到的匹配数据
@@ -13,41 +13,41 @@ public class searchData {
         BufferedWriter bw = new BufferedWriter(outStream);
         String valueString = null;
         int flag = -1;
-         */
+
 
         //多线程
-        int numThreads = Runtime.getRuntime().availableProcessors();
-        Thread[] threads = new Thread[numThreads];
-        ParallelGroupFileTask[] tasks = new ParallelGroupFileTask[numThreads];
+//        int numThreads = Runtime.getRuntime().availableProcessors();
+//        Thread[] threads = new Thread[numThreads];
+//        ParallelGroupFileTask[] tasks = new ParallelGroupFileTask[numThreads];
+//
+//        for (int i = 0; i < numThreads; i++) {
+//            tasks[i] = new ParallelGroupFileTask(filename, key);
+//            threads[i] = new Thread(tasks[i]);
+//            threads[i].start();
+//        }
+//        boolean finish = false;
+//        int numFinished = 0;
+//        while (!finish) {
+//            numFinished = 0;
+//            for (int i = 0; i < threads.length; i++) {
+//                if (threads[i].getState() == Thread.State.TERMINATED) {
+//                    numFinished++;
+//                    if (tasks[i].isFinish(valueString,br)) {
+//                        finish = true;
+//                    }
+//                }
+//            }
+//            if (numFinished == threads.length) {
+//                finish = true;
+//            }
+//            if (numFinished != threads.length) {
+//                for (Thread thread : threads) {
+//                    thread.interrupt();
+//                }
+//            }
+//        }
 
-        for (int i = 0; i < numThreads; i++) {
-            tasks[i] = new ParallelGroupFileTask(filename, key);
-            threads[i] = new Thread(tasks[i]);
-            threads[i].start();
-        }
-        boolean finish = false;
-        int numFinished = 0;
-        while (!finish) {
-            numFinished = 0;
-            for (int i = 0; i < threads.length; i++) {
-                if (threads[i].getState() == Thread.State.TERMINATED) {
-                    numFinished++;
-                    if (tasks[i].isFinish(valueString,br)) {
-                        finish = true;
-                    }
-                }
-            }
-            if (numFinished == threads.length) {
-                finish = true;
-            }
-            if (numFinished != threads.length) {
-                for (Thread thread : threads) {
-                    thread.interrupt();
-                }
-            }
-        }
 
-        /*
         //按行读取数据
         while ((valueString = br.readLine()) != null){
             flag = valueString.indexOf(key);
@@ -57,7 +57,8 @@ public class searchData {
             }
         }
         bw.close();
-         */
+        br.close();
+
     }
 
 
