a982d1579f2305dc76ba3e65ac9c8d781aafa194
yi_jin <1837935416@qq.com>
2022-10-06 19:44:09 +0800

    justify ansjCutData

--- src/main/java/ansjCutData.java
+++ src/main/java/ansjCutData.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class dealData {
+public class ansjCutData {
     //按照行来读取数据
     static void DealFile() throws IOException{
         String fileName = "files/preResult.txt";
@@ -23,23 +23,6 @@ public class dealData {
         }
     }
 
-    public static void localDic(){
-        try {
-            //读取的是根目录下的
-            Forest rootForest = Library.makeForest("library/userLibrary.dic");
-            System.out.println(rootForest.toMap());
-            String str = "阿里巴巴是我喜欢的企业！初音未来是我最喜欢的虚拟歌手！";
-            //加载字典文件,取的是resource下的
-            Forest resourceForest=Library.makeForest(Objects.requireNonNull(dealData.class.getResourceAsStream("/library/userLibrary.dic")));
-            Result result=ToAnalysis.parse(str, resourceForest);//传入forest
-            List<Term> termList = result.getTerms();
-            for(Term term:termList){
-                System.out.println(term.getName());
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
     //停用词移除判断
     static boolean ifRemoveStopWords(String str) throws IOException{
         boolean flag = true;
@@ -57,7 +40,43 @@ public class dealData {
         return flag;
     }
 
-    static void deal_clean() throws IOException{
+    //采用自定义词典下的分词
+    public static void localDic_cutAndclean() throws IOException{
+        String fileName = "src/main/resources/files/cleanResult.txt";
+        //定义输出流，写入搜索到的匹配数据
+        OutputStreamWriter outStream = new OutputStreamWriter(new FileOutputStream(new File("src/main/resources/files/ansj_localDic_cutResult.txt")), "UTF-8");
+        BufferedWriter bw = new BufferedWriter(outStream);
+        //定义变量
+        String str = null;
+        String index = null;
+
+        try(Scanner sc = new Scanner(new FileReader(fileName))) {
+            //定义自定义词典并加载字典文件
+            Forest rootForest = Library.makeForest("src/main/resources/library/userLibrary.dic");
+            Forest resourceForest=Library.makeForest(Objects.requireNonNull(new FileInputStream(new File("src/main/resources/library/userLibrary.dic"))));
+            while (sc.hasNextLine()) {
+                str = sc.nextLine();
+                System.out.println(str);
+                Result result = ToAnalysis.parse(str, resourceForest);//传入forest
+                List<Term> termList = result.getTerms();
+                for(Term term:termList){
+                    index = term.getName();
+                    System.out.println(index);
+                    if(index.length()>=2 && ifRemoveStopWords(index)){
+                        bw.append(index);
+                        bw.newLine();
+                    }
+                }
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        bw.close();
+    }
+
+    //采用原始词典下的分词清洗
+    static void cut_clean() throws IOException{
         String fileName = "src/main/resources/files/cleanResult.txt";
         //定义输出流，写入搜索到的匹配数据
         OutputStreamWriter outStream = new OutputStreamWriter(new FileOutputStream(new File("src/main/resources/files/ansj_cutResult.txt")), "UTF-8");
@@ -105,14 +124,17 @@ public class dealData {
         bw.close();
     }
     public static void main(String[] args) throws IOException {
-        deal_clean();
-        localDic();
+        //测试分词时间
+        long timeStart = System.currentTimeMillis();
+        cut_clean();
+        long timeEnd = System.currentTimeMillis();
+        System.out.printf("采用ansj分词的执行时长：%d 毫秒.\n", (timeEnd - timeStart));
 
-//        String str = "这里作者使用了注意力的方法进行了特征融合，在这里作者主要参考的是使用SENet网络的结构。这是因为SENet计算attention的方式就是把每个通道的像素值做一个平均之后，然后经过一系列操作后，用sigmoid函数归一化。这样的操作对于大尺度的目标还是有效果的，但是对于小目标效果就不太好，所以的话本文就提出了多尺度的方法来计算。其实我个人也进行了一些注意力的特征融合操作。其实效果还是不错的。\n";
-//        System.out.println(ToAnalysis.parse(str));
-//        System.out.println(NlpAnalysis.parse(str));
-//        Forest dic1 = new Forest();
-//        Library.insertWord(dic1,new Value("注意力","define","1000"));
-//        System.out.println(NlpAnalysis.parse(str,dic1));
+        //测试分词时间
+//        long timeStart = System.currentTimeMillis();
+//        localDic_cutAndclean();
+//        long timeEnd = System.currentTimeMillis();
+//        System.out.printf("采用ansj自定义词典分词的执行时长：%d 毫秒.\n", (timeEnd - timeStart));
+
     }
 }
