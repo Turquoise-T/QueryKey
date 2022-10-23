d52fe039a24131253b9851099e6de0cc32cdb21f
yi_jin <1837935416@qq.com>
2022-10-23 23:00:40 +0800

    new compkey

--- src/main/java/compkey/util.java
+++ src/main/java/compkey/util.java
@@ -79,6 +79,74 @@ public class util {
         return entrys;
     }
 
+
+    public static Map<String,Integer> sort(Map<String,Integer> map){
+        //获取entrySet
+        Set<Map.Entry<String, Integer>> mapEntries = map.entrySet();
+        //使用链表来对集合进行排序，使用LinkedList，利于插入元素
+        List<Map.Entry<String, Integer>> result = new LinkedList<>(mapEntries);
+        //自定义比较器来比较链表中的元素
+        Collections.sort(result, new Comparator<Map.Entry<String, Integer>>() {
+            //基于entry的值（Entry.getValue()），来排序链表
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+        //将排好序的存入到LinkedHashMap(可保持顺序)中，需要存储键和值信息对到新的映射中。
+        Map<String, Integer> linkMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> newEntry : result) {
+            linkMap.put(newEntry.getKey(), newEntry.getValue());
+        }
+        return linkMap;
+    }
+
+    public static Map<String,Double> sortComp(Map<String,Double> map){
+        //获取entrySet
+        Set<Map.Entry<String, Double>> mapEntries = map.entrySet();
+        //使用链表来对集合进行排序，使用LinkedList，利于插入元素
+        List<Map.Entry<String, Double>> result = new LinkedList<>(mapEntries);
+        //自定义比较器来比较链表中的元素
+        Collections.sort(result, new Comparator<Map.Entry<String, Double>>() {
+            //基于entry的值（Entry.getValue()），来排序链表
+            @Override
+            public int compare(Map.Entry<String, Double> o1, Map.Entry<String, Double> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+        //将排好序的存入到LinkedHashMap(可保持顺序)中，需要存储键和值信息对到新的映射中。
+        Map<String, Double> linkMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Double> newEntry : result) {
+            linkMap.put(newEntry.getKey(), newEntry.getValue());
+        }
+        return linkMap;
+    }
+
+    public static int compSearch(String seed,String mid,String fileOut) throws IOException {
+        //定义搜索记录数
+        int counter = 0;
+        //定义输出流，写入搜索到的匹配数据
+        OutputStreamWriter outStream = new OutputStreamWriter(new FileOutputStream(new File(fileOut)), "UTF-8");
+        BufferedWriter bw = new BufferedWriter(outStream);
+        try(Scanner sc = new Scanner(new FileReader("src/main/resources/compkeyFiles/cleanResult.txt"))) {
+            String line;
+            while (sc.hasNextLine()) {
+                line = sc.nextLine();
+                //包含中介但不包含种子
+                if(!line.contains(seed)&&line.contains(mid)){
+                    counter++;
+                    bw.append(line);
+                    bw.newLine();
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        bw.close();
+        System.out.println("=========="+mid+"的相关搜索记录查询已完成==========");
+        return counter;
+    }
+
     public static String getFilePath(String FileName){
         String root = System.getProperty("user.dir");
         String subPath = "src/main/resources/files/";
